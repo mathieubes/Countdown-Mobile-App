962e06ec4939cb9f94ea4344b05faeabb2234087
Mathieu Bes <mathieu.bes@renovationman.com>
2021-11-26 17:07:33 +0100

    First commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator></Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
